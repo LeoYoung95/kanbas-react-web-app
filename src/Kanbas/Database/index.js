59fd9b025a4eae63355e7e246f9bbe57e5cf3288
yangxu95 <yang.xu6@northeastern.edu>
2023-11-16 16:41:27 -0800

    Modify URL

--- src/Kanbas/Database/index.js
+++
@@ -1,9 +0,0 @@
-import users from "./users.json";
-import enrollments from "./enrollments.json";
-import grades from "./grades.json";
-export default {
-    users,
-    enrollments,
-    grades
-};
-
